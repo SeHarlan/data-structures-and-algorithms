cca562287573459b23e24f943d07c9204eead183
Scott Harlan <seharlan@gmail.com>
2020-03-09 18:12:45 -0700

    planning added, definitely didnt plan after i did it

--- challenges/array-map/array-map.js
+++ challenges/array-map/array-map.js
@@ -1,6 +1,9 @@
 function map(arr, func) {
+    //create new array to be returned
     const newArray = [];
+    //itterate through each item in the origional array
     for(let i = 0; i < arr.length; i++) {
+        //apply the callback and assign the result to the new array
         newArray[i] = func(arr[i])
     }
     return newArray

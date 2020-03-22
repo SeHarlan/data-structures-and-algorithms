78a3d167b73a7b3b1b6339567d4ee7d341d30321
Scott Harlan <seharlan@gmail.com>
2020-03-22 16:04:53 -0700

    11 insertion methods complete

--- challenges/linked-list/linked-list.js
+++ challenges/linked-list/linked-list.js
@@ -33,6 +33,49 @@ class LinkedList {
     }
     return string
   }
+  //==========11-insertion methods
+  append(value) {
+    //adds vlaue to the end of the list
+    const newNode = new Node(value)
+    let node = this.head
+    while(node) {
+      if (node.next === null) {
+        node.next = newNode;
+        return this.head
+      }
+      node = node.next
+    }
+  }
+  insertBefore(value, newVal) {
+    //inserts data before the value
+    let node = this.head
+    if (node.data === value) return this.insert(newVal);
+    const newNode = new Node(newVal)
+    while(node) {
+      if (node.next.data === value ) {
+        // node/test2 (insert here) node/test3/value
+        newNode.next = node.next;
+        node.next = newNode;
+        return this.head
+      }
+      node = node.next
+    }
+  }
+  insertAfter(value, newVal) {
+    //inserts data after given value
+    let node = this.head
+    const newNode = new Node(newVal)
+    while(node) {
+      if (node.data === value ) {
+        // node/test2  node/test3/value (insert here) node/test4
+        newNode.next = node.next;
+        node.next = newNode;
+        return this.head
+      }
+      node = node.next
+    }
+    return this.head
+  }
 }
 
 module.exports = {
--- challenges/linked-list/linked-list.test.js
+++ challenges/linked-list/linked-list.test.js
@@ -2,35 +2,67 @@ const {
   LinkedList
 } = require('./linked-list.js');
 const { DoubleLinkedList } = require('./double-linked.js')
-const ll = new LinkedList
 
 
-  describe('linked list', () => {
-    it('instantiates an empty ll', () => {
-      expect(ll).toEqual({ head: null })
-    });
-    it('can properly instert into ll', () => {
-      ll.insert('test 1')
-      expect(ll.head).toEqual({ data: 'test 1', next: null })
-    });
-    it('head points to first node', () => {
-      expect(ll.head.data).toEqual('test 1')
-    });
-    it('can insert mutiple items', () => {
-      ll.insert('test 2')
-      ll.insert('test 3')
-      expect(ll.head).toEqual({ data: 'test 3', next: { data: 'test 2', next: { data: 'test 1', next: null}}})
-    });
-    it('returns true when finding a value', () => {
-      expect(ll.includes('test 2')).toEqual(true)
-    });
-    it('returns false when theres no item', () => {
-      expect(ll.includes('bad test')).toEqual(false)
-    });
-    it('returns a collection', () => {
-      expect(ll.toString()).toEqual('test 3, test 2, test 1')
-    });
-    console.log('===============================================')
+console.log('===============================================')
+
+  describe('Linked List', () => {
+    describe('initial implimentaion', () => {
+
+      const ll = new LinkedList
+
+      it('instantiates an empty ll', () => {
+        expect(ll).toEqual({ head: null })
+      });
+      it('can properly instert into ll', () => {
+        ll.insert('test 1')
+        expect(ll.head).toEqual({ data: 'test 1', next: null })
+      });
+      it('head points to first node', () => {
+        expect(ll.head.data).toEqual('test 1')
+      });
+      it('can insert mutiple items', () => {
+        ll.insert('test 2')
+        ll.insert('test 3')
+        expect(ll.head).toEqual({ data: 'test 3', next: { data: 'test 2', next: { data: 'test 1', next: null}}})
+      });
+      it('returns true when finding a value', () => {
+        expect(ll.includes('test 2')).toEqual(true)
+      });
+      it('returns false when theres no item', () => {
+        expect(ll.includes('bad test')).toEqual(false)
+      });
+      it('returns a collection', () => {
+        expect(ll.toString()).toEqual('test 3, test 2, test 1')
+      });
+    })
+
+
+    describe('11 - insertions', () => {
+
+      const insertionLL = new LinkedList
+      insertionLL.insert('test 2')
+      insertionLL.insert('test 1')
+      it('adds node to end of list', () => {
+        expect(insertionLL.append('test 3')).toEqual({ data: 'test 1', next: { data: 'test 2', next: { data: 'test 3', next: null}}})
+      });
+      it('adds multiple nodes to the end of list', () => {
+        insertionLL.append('test 4')
+        expect(insertionLL.head).toEqual({ data: 'test 1', next: { data: 'test 2', next: { data: 'test 3', next: { data: 'test 4', next: null}}}})
+      })
+      it('inserts node before a specified node', () => {
+        expect(insertionLL.insertBefore('test 3', 'before 3')).toEqual({ data: 'test 1', next: { data: 'test 2', next: { data: 'before 3', next :{ data: 'test 3', next: { data: 'test 4', next: null}}}}})
+      })
+      it('inserts node before fisrt node in list', () => {
+        expect(insertionLL.insertBefore('test 1', 'before 1')).toEqual({ data: 'before 1', next: {data: 'test 1', next: { data: 'test 2', next: { data: 'before 3', next :{ data: 'test 3', next: { data: 'test 4', next: null}}}}}})
+      })
+      it('inserts node after specified node', () => {
+        expect(insertionLL.insertAfter('test 3', 'after 3')).toEqual({ data: 'before 1', next: {data: 'test 1', next: { data: 'test 2', next: { data: 'before 3', next :{ data: 'test 3', next: {data: 'after 3', next: { data: 'test 4', next: null}}}}}}})
+      })
+      it('inserts node after the last node in a list', () => {
+        expect(insertionLL.insertAfter('test 4', 'last test')).toEqual({ data: 'before 1', next: {data: 'test 1', next: { data: 'test 2', next: { data: 'before 3', next :{ data: 'test 3', next: {data: 'after 3', next: { data: 'test 4', next: {data: 'last test', next: null }}}}}}}})
+      })
+    })
   })
 
 
@@ -80,5 +112,4 @@ const ll = new LinkedList
     it('returns a collection in  double', () => {
       expect(dll.toString()).toEqual('test 3, test 2, test 1')
     });
-    console.log('===============================================')
   })

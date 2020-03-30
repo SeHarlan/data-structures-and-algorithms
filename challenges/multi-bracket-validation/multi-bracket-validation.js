f0a9fc1b0dc964862dc1d57d2eea3304217cc413
Scott Harlan <seharlan@gmail.com>
2020-03-29 19:48:50 -0700

    all tests exepct {(}) pass

--- challenges/multi-bracket-validation/multi-bracket-validation.js
+++ challenges/multi-bracket-validation/multi-bracket-validation.js
@@ -1,7 +1,27 @@
 function multiBracketValidation(input) {
+  arr = input.split('')
 
+ 
+  let stack = []
+
+  const openingBrackets = ['[', '{', '(']
+  const closingBrackets = [']', '}', ')']
+
+  arr.forEach(character => {
+    
+    if(closingBrackets.indexOf(character) > -1) {
+      const matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(character)]
+
+      if (stack.length === 0 || (stack.pop() !== matchingOpeningBracket)) return 
+    } else {
+      if (!openingBrackets.includes(character) && !closingBrackets.includes(character)) return
+      stack.push(character)
+    }
+  });
+  return (stack.length === 0)
 }
 
 module.exports = {
   multiBracketValidation
 }
+
